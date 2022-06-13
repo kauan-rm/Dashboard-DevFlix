9df9953afebc978aac1ad8b84f561afc6991b628
kauan-rm <rmartins.kauan@gmail.com>
2022-06-13 02:20:01 -0300

    atualização

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,19 +6,27 @@ from ..models import User
 from app import db
 # Termino Import
 
-@auth.route("/login", methods=['GET','POST']) # Rota de login
-def login():  
+@auth.route("/login") # Rota de login
+def login():
+    return render_template("login.html")
+
+@auth.route("/do_login", methods=['POST']) # Rota de login
+def do_login():  
     if request.form:
         user = User.query.filter_by(email=request.form['email']).first()
         if user and user.senha_hash == request.form['senha']: #verify_password(request.form['senha']):
             login_user(user)
-            return "Funcionou" #redirect(url_for('main.index'))
-        return request.form['senha'] #redirect(url_for('main.index'))
+            return redirect(url_for('main.index'))
+        return redirect(url_for('auth.login'))
     return render_template("login.html") # Renderiza arquivo html pasta templates
 
 
-@auth.route('/registro', methods=['GET','POST']) # Rota de registro 
+@auth.route('/registro') # Rota de registro 
 def registro():
+    return render_template("register.html")
+
+@auth.route('/do_register', methods=['POST']) # Rota de registro 
+def do_register():
     if request.form and request.form['senha']==request.form['conf_senha']:    
         user = User()
         user.nome = request.form['nome']
@@ -27,6 +35,7 @@ def registro():
         user.senha_hash = request.form['senha']
         db.session.add(user)
         db.session.commit()
+        return "dados inseridos no banco de dados" # Alan aqui sua parte
     return render_template("register.html")  # Renderiza arquivo html pasta templates
     
 
